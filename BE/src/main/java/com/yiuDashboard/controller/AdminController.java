b91108669e136ec692f4e96debc7ebc006cccbe7
KwangWook An <akw2080@naver.com>
2025-07-15 15:43:21 +0900

    전임교원 1인당 학생 수, 강의 담당 비율, 충원율/입시경갱률/등록률 추가

--- BE/src/main/java/com/yiuDashboard/controller/AdminController.java
+++ BE/src/main/java/com/yiuDashboard/controller/AdminController.java
@@ -4,7 +4,6 @@ import com.yiuDashboard.service.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/admin/faculty")
@@ -13,13 +12,53 @@ public class AdminController {
 
     private final AdminService adminService;
 
-    @GetMapping("/compare/{year}")
+    @GetMapping("/ensure/compare/{year}")
     public String getComparisonFullTimeFacultyEnsureCrntSt(@PathVariable int year, Authentication authentication) {
         return adminService.getComparisonFullTimeFacultyEnsureCrntSt(year).block();
     }
 
-    @GetMapping("/region")
-    public String getRegionalFullTimeFacultyEnsureCrntSt(@PathVariable int year, Authentication authentication) {
-        return adminService.getRegionalFullTimeFacultyEnsureCrntSt(year).block();
+    @GetMapping("/ensure/region/{schlDivCd}")
+    public String getRegionalFullTimeFacultyEnsureCrntSt(@PathVariable int schlDivCd, Authentication authentication) {
+        return adminService.getRegionalFullTimeFacultyEnsureCrntSt(schlDivCd).block();
+    }
+
+    @GetMapping("/ensure/notice/{year}")
+    public String getNoticeFullTimeFacultyEnsureRate(@PathVariable int year, Authentication authentication) {
+        return adminService.getNoticeFullTimeFacultyEnsureRate(year).block();
+    }
+
+    @GetMapping("/stdntNum/compare/{year}")
+    public String getComparisonFullTimeFacultyForPersonStudentNumberEnrolledStudent(@PathVariable int year, Authentication authentication) {
+        return adminService.getComparisonFullTimeFacultyForPersonStudentNumberEnrolledStudent(year).block();
+    }
+
+    @GetMapping("/stdntNum/region/{schlDivCd}")
+    public String getRegionalFullTimeFacultyForPersonStudentNumberEnrolledStudent(@PathVariable int schlDivCd, Authentication authentication) {
+        return adminService.getRegionalFullTimeFacultyForPersonStudentNumberEnrolledStudent(schlDivCd).block();
+    }
+
+    @GetMapping("/lecRatio/compare")
+    public String getComparisonLectureChargeRatio(@PathVariable int year, Authentication authentication) {
+        return adminService.getComparisonLectureChargeRatio(year).block();
+    }
+
+    @GetMapping("/lecRatio/region/{schlDivCd}")
+    public String getRegionalLectureChargeRatio(@PathVariable int schlDivCd, Authentication authentication) {
+        return adminService.getRegionalLectureChargeRatio(schlDivCd).block();
+    }
+
+    @GetMapping("/enrolledStdnt/compare/{year}")
+    public String getComparisonEnrolledStudentEnsureRate(@PathVariable int year, Authentication authentication) {
+        return adminService.getComparisonEnrolledStudentEnsureRate(year).block();
+    }
+
+    @GetMapping("/freshCompetite/compare/{year}")
+    public String getComparisonInsideFixedNumberFreshmanCompetitionRate(@PathVariable int year, Authentication authentication) {
+        return adminService.getComparisonInsideFixedNumberFreshmanCompetitionRate(year).block();
+    }
+
+    @GetMapping("/register/compare/{year}")
+    public String getComparisonEntranceModelLastRegistrationRatio(@PathVariable int year, Authentication authentication) {
+        return adminService.getComparisonEntranceModelLastRegistrationRatio(year).block();
     }
 }
--- BE/src/main/java/com/yiuDashboard/service/AdminService.java
+++ BE/src/main/java/com/yiuDashboard/service/AdminService.java
@@ -31,12 +31,108 @@ public class AdminService {
                 .bodyToMono(String.class);
     }
 
-    public Mono<String> getRegionalFullTimeFacultyEnsureCrntSt(int year) {
+    public Mono<String> getRegionalFullTimeFacultyEnsureCrntSt(int schlDivCd) {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/EducationResearchService/getRegionalFullTimeFacultyEnsureCrntSt")
                         .queryParam("ServiceKey", serviceKey)
-                        .queryParam("schlDivCd", 02)
+                        .queryParam("schlDivCd", schlDivCd)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    public Mono<String> getNoticeFullTimeFacultyEnsureRate(int year) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/EducationResearchService/getNoticeFullTimeFacultyEnsureRate")
+                        .queryParam("ServiceKey", serviceKey)
+                        .queryParam("schlId", schoolId)
+                        .queryParam("svyYr", year)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    public Mono<String> getComparisonFullTimeFacultyForPersonStudentNumberEnrolledStudent(int year) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/EducationResearchService/getComparisonFullTimeFacultyForPersonStudentNumberEnrolledStudent")
+                        .queryParam("ServiceKey", serviceKey)
+                        .queryParam("schlId", schoolId)
+                        .queryParam("svyYr", year)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    public Mono<String> getRegionalFullTimeFacultyForPersonStudentNumberEnrolledStudent(int schlDivCd) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/EducationResearchService/getRegionalFullTimeFacultyForPersonStudentNumberEnrolledStudent")
+                        .queryParam("ServiceKey", serviceKey)
+                        .queryParam("schlDivCd", schlDivCd)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    public Mono<String> getComparisonLectureChargeRatio(int year) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/EducationResearchService/getComparisonLectureChargeRatio")
+                        .queryParam("ServiceKey", serviceKey)
+                        .queryParam("pageNo", 1)
+                        .queryParam("numOfRows", 999)
+                        .queryParam("schlId", schoolId)
+                        .queryParam("svyYr", year)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    public Mono<String> getRegionalLectureChargeRatio(int schlDivCd) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/EducationResearchService/getRegionalLectureChargeRatio")
+                        .queryParam("ServiceKey", serviceKey)
+                        .queryParam("schlDivCd", schlDivCd)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    public Mono<String> getComparisonEnrolledStudentEnsureRate(int year) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/StudentService/getComparisonEnrolledStudentEnsureRate")
+                        .queryParam("ServiceKey", serviceKey)
+                        .queryParam("schlId", schoolId)
+                        .queryParam("svyYr", year)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    public Mono<String> getComparisonInsideFixedNumberFreshmanCompetitionRate(int year) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/StudentService/getComparisonInsideFixedNumberFreshmanCompetitionRate")
+                        .queryParam("ServiceKey", serviceKey)
+                        .queryParam("schlId", schoolId)
+                        .queryParam("svyYr", year)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    public Mono<String> getComparisonEntranceModelLastRegistrationRatio(int year) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/StudentService/getComparisonEntranceModelLastRegistrationRatio")
+                        .queryParam("ServiceKey", serviceKey)
+                        .queryParam("schlId", schoolId)
+                        .queryParam("svyYr", year)
                         .build())
                 .retrieve()
                 .bodyToMono(String.class);
