4dffe19204695366416490f6eee146954646912e
soons <soons@example.com>
2025-07-25 01:23:42 +0900

    feat: 전공과 교양 성적 API 추가

--- BE/src/main/java/com/yiuDashboard/repository/DepartmentOutcomeRepository.java
+++ BE/src/main/java/com/yiuDashboard/repository/DepartmentOutcomeRepository.java
@@ -14,4 +14,10 @@ public interface DepartmentOutcomeRepository extends JpaRepository<DepartmentOut
 
     //재학생 수 기준 내림차순 정렬
     List<DepartmentOutcome> findAllByOrderByEnrolledStudentsDesc();
+
+    //특정 학과의 전공/교양 성적 평균 및 차이 조회
+    List<DepartmentOutcome> findByDepartmentNameOrderByMajorVsGeneralDifferenceDesc(String departmentName);
+
+    //전체 학과의 전공 - 교양 성적 차이 내림차순 정렬
+    List<DepartmentOutcome> findAllByOrderByMajorVsGeneralDifferenceDesc();
 }
